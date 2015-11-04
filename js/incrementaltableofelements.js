f9ca49242443f168e3eb733b4c2eb8926d1f2f12
angarg12 <angarg12@gmail.com>
2015-11-04 15:16:43 +0200

    Extracted some of the values from the player to global variables

--- js/incrementaltableofelements.js
+++ js/incrementaltableofelements.js
@@ -5,30 +5,10 @@ function($scope,$document,$interval,$sce,$filter,$timeout) {
 		$scope.Math = window.Math;
 		
 		const startPlayer = {
-			tabs:{'Elements':{visible:function(){
-							return true;
-						},
-						has_new:false,
-						order:0},
-				'Encyclopedia':{visible:function(){
-							return true;
-						},
-						has_new:true,
-						order:1},
-				'Periodic Table':{visible:function(){
-							return false;
-						},
-						has_new:false,
-						order:2},
-				'Options':{visible:function(){
-							return true;
-						},
-						has_new:false,
-						order:3}
-			},		
+			unlocks: {
+			},
 			elements: {
 				'H':{
-					name:'Hydrogen',
 					level: [10,4,1,2,0],
 					upgrades:[
 						{name:'Upgrade 1',
@@ -43,20 +23,9 @@ function($scope,$document,$interval,$sce,$filter,$timeout) {
 							price:100,
 							description:"Do this and that",
 							purchased:false}],
-					visible:function(){
-							return true;
-						},
-					has_new:false,
-					order:1
 				},'O':{
-					name:'Oxygen',
 					level: [15,1,0,0],
-					visible:function(){
-							return $scope.player.elements.O.unlocked;
-						},
-					has_new:true,
-					unlocked:true,
-					order:7
+					unlocked:true
 				}
 			},
 			resources:{
@@ -93,7 +62,7 @@ function($scope,$document,$interval,$sce,$filter,$timeout) {
 						visible:function(){
 							return $scope.current_element === "O";
 						},
-						order:0,
+						order:700,
 						ratio:0.9976
 					},
 					'17O':{ 
@@ -102,7 +71,7 @@ function($scope,$document,$interval,$sce,$filter,$timeout) {
 						visible:function(){
 							return $scope.current_element === "O";
 						},
-						order:1,
+						order:701,
 						ratio:0.00039
 					},
 					'18O':{ 
@@ -111,7 +80,7 @@ function($scope,$document,$interval,$sce,$filter,$timeout) {
 						visible:function(){
 							return $scope.current_element === "O";
 						},
-						order:2,						
+						order:702,						
 						ratio:0.00201 
 					},
 					'e-':{ 
@@ -120,7 +89,7 @@ function($scope,$document,$interval,$sce,$filter,$timeout) {
 						visible:function(){
 							return true;
 						},
-						order:0
+						order:20000
 					},
 					'n':{ 
 						number:0,
@@ -128,7 +97,7 @@ function($scope,$document,$interval,$sce,$filter,$timeout) {
 						visible:function(){
 							return true;
 						},
-						order:1
+						order:20001
 					},
 					'p':{ 
 						number:0,
@@ -136,7 +105,7 @@ function($scope,$document,$interval,$sce,$filter,$timeout) {
 						visible:function(){
 							return true;
 						},
-						order:2
+						order:20002
 					},
 					'energy':{ 
 						number:0,
@@ -144,11 +113,134 @@ function($scope,$document,$interval,$sce,$filter,$timeout) {
 						visible:function(){
 							return true;
 						},
-						order:3
+						order:20003
 					}
 				}
 			};
 		
+		$scope.elements = {
+				'H':{
+					name:'Hydrogen',
+					isotopes:['2H','3H'],
+					upgrades:[
+						{name:'Upgrade 1',
+							price:1,
+							description:"Do this and that",
+							purchased:true},
+						{name:'Upgrade 2',
+							price:10,
+							description:"Do this and that",
+							purchased:false},
+						{name:'Upgrade 3',
+							price:100,
+							description:"Do this and that",
+							purchased:false}],
+					visible:function(){
+							return true;
+						},
+					has_new:false,
+					order:1
+				},'O':{
+					name:'Oxygen',
+					visible:function(){
+							return $scope.player.elements.O.unlocked;
+						},
+					has_new:true,
+					order:7
+				}
+		};
+		
+		$scope.resources = {
+					'H':{ 
+						visible:function(){
+							return $scope.current_element === "H";
+						},
+						order:0,
+						ratio:0.999884
+					},
+					'2H':{ 
+						visible:function(){
+							return $scope.current_element === "H";
+						},
+						order:1,
+						ratio:0.000115
+					},
+					'3H':{ 
+						visible:function(){
+							return $scope.current_element === "H";
+						},
+						order:2,						
+						ratio:0.000001
+					},
+					'O':{ 
+						visible:function(){
+							return $scope.current_element === "O";
+						},
+						order:700,
+						ratio:0.9976
+					},
+					'17O':{ 					
+						visible:function(){
+							return $scope.current_element === "O";
+						},
+						order:701,
+						ratio:0.00039
+					},
+					'18O':{ 						
+						visible:function(){
+							return $scope.current_element === "O";
+						},
+						order:702,						
+						ratio:0.00201 
+					},
+					'e-':{ 
+						visible:function(){
+							return true;
+						},
+						order:20000
+					},
+					'n':{ 
+						visible:function(){
+							return true;
+						},
+						order:20001
+					},
+					'p':{ 
+						visible:function(){
+							return true;
+						},
+						order:20002
+					},
+					'energy':{ 
+						visible:function(){
+							return true;
+						},
+						order:20003
+					}
+		};
+		
+		$scope.tabs = {'Elements':{visible:function(){
+							return true;
+						},
+						has_new:false,
+						order:0},
+				'Encyclopedia':{visible:function(){
+							return true;
+						},
+						has_new:true,
+						order:1},
+				'Periodic Table':{visible:function(){
+							return false;
+						},
+						has_new:false,
+						order:2},
+				'Options':{visible:function(){
+							return true;
+						},
+						has_new:false,
+						order:3}
+			};
+			
 		var resourceHTML = {
 					'2H':'<sup>2</sup>H',
 					'3H':'<sup>3</sup>H',
@@ -159,27 +251,27 @@ function($scope,$document,$interval,$sce,$filter,$timeout) {
 		
 		var priceIncrease = 1.15;
         var generatorBasePrice = [15,
-                                         100,
-                                         500,
-                                         3000,
-                                         10000,
-                                         40000,
-                                         200000,
-                                         1666666,
-                                         123456789,
-                                         3999999999,
-                                         75000000000];
+                                 100,
+                                 500,
+                                 3000,
+                                 10000,
+                                 40000,
+                                 200000,
+                                 1666666,
+                                 123456789,
+                                 3999999999,
+                                 75000000000];
         $scope.generatorPower = [0.1,
-                                        0.5,
-                                        4,
-                                        10,
-                                        40,
-                                        100,
-                                        400,
-                                        6666,
-                                        98765,
-                                        999999,                                        		
-                                        10000000];
+                                0.5,
+                                4,
+                                10,
+                                40,
+                                100,
+                                400,
+                                6666,
+                                98765,
+                                999999,                                        		
+                                10000000];
 		
 		cache = {};
 		$scope.current_tab = "Elements";
@@ -198,7 +290,7 @@ function($scope,$document,$interval,$sce,$filter,$timeout) {
 		
 		$scope.isCostMet = function(element, index) {return false;};
 
-		$scope.filterAndOrder = function(map) {	
+		$scope.filterAndOrder = function(map, table) {	
 			if(map == undefined) return;
 			hash = JSON.stringify(map).hashCode();
 			if(cache[hash] != null){
@@ -206,7 +298,10 @@ function($scope,$document,$interval,$sce,$filter,$timeout) {
 			}	
 			var array = [];
 			for(var objectKey in map) {
-				if(map[objectKey].visible()){
+				if(Array.isArray(map)){
+					objectKey = map[objectKey];
+				}
+				if(table[objectKey].visible()){
 					var object = {};
 					object.name = objectKey;
 					object.value = map[objectKey];
@@ -214,8 +309,8 @@ function($scope,$document,$interval,$sce,$filter,$timeout) {
 				}
 			}
 			array.sort(function(a, b){
-				n = parseInt(a.value.order);
-				m = parseInt(b.value.order);
+				n = parseInt(table[a.name].order);
+				m = parseInt(table[b.name].order);
 				return n - m;
 			});
 			cache[hash] = array;
