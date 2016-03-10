9210d779eeb9a64a5156f3706f2af95c4a3245bc
Andrés <angarg12@gmail.com>
2016-03-10 21:21:43 +0100

    Upgrades buffed

--- js/incremental_table_elements.js
+++ js/incremental_table_elements.js
@@ -1,7 +1,7 @@
 angular.module('incremental',['ngAnimate'])
 .controller('IncCtrl',['$scope','$document','$interval', '$sce', '$filter', '$timeout', 
 function($scope,$document,$interval,$sce,$filter,$timeout) { 
-		$scope.version = '0.9.4';
+		$scope.version = '0.9.5';
 		$scope.Math = window.Math;
 		
 		// Polyfill for some browsers
