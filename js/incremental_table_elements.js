365f04ed42ecc36d61e820c833d50b02e834d899
Andrés <angarg12@gmail.com>
2016-03-10 23:30:18 +0100

    Added padding to the bottom so that toast don't get on the way

--- js/incremental_table_elements.js
+++ js/incremental_table_elements.js
@@ -1,7 +1,7 @@
 angular.module('incremental',['ngAnimate'])
 .controller('IncCtrl',['$scope','$document','$interval', '$sce', '$filter', '$timeout', 
 function($scope,$document,$interval,$sce,$filter,$timeout) { 
-		$scope.version = '0.9.7';
+		$scope.version = '0.9.8';
 		$scope.Math = window.Math;
 		
 		// Polyfill for some browsers
