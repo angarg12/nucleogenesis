91011b6d8e40532a195bdc05a373021cf6d7ee50
Andrés <angarg12@gmail.com>
2017-04-10 22:39:29 +0200

    More Gulp tasks

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,12 +1,11 @@
 // Include gulp
 var gulp = require('gulp');
+var del = require('del');
 
 // Include plugins
 var plugins = require('gulp-load-plugins')();
 
 var Server = require('karma').Server;
-
-console.log(plugins);
 
 gulp.task('karma', function (done) {
   new Server({
@@ -36,13 +35,44 @@ gulp.task('protractor', ['connect', 'webdriver_update'], function() {
     .pipe(plugins.protractor.protractor({
         configFile: "protractor/protractor.conf.js"
     }))
-    .on('error', function(e) { throw e });
+    .on('error', function(e) { throw e; });
   });
 
 gulp.task('disconnect', ['connect', 'protractor'], function() {
   return plugins.connect.serverClose();
 });
 
+gulp.task('jshint',function(){
+  gulp.src('src/**/*.js')
+  .pipe(plugins.jshint())
+  .pipe(plugins.jshint.reporter('default'));
+});
+
+gulp.task('clean',function(){
+  return del(['dist','build']);
+});
+
+gulp.task('htmlmin', function() {
+  return gulp.src('src/**/*.html')
+    .pipe(plugins.htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest('build/'));
+});
+
+gulp.task('uglify', function() {
+  return gulp.src('src/**/*.js')
+    .pipe(plugins.uglify())
+    .pipe(gulp.dest('build/'));
+});
+
+gulp.task('cleanCss', function() {
+  return gulp.src('styles/*.css')
+    .pipe(plugins.cleanCss())
+    .pipe(gulp.dest('build/'));
+});
+
+gulp.task('minify', ['uglify', 'htmlmin', 'cleanCss']);
+
+// public tasks
 gulp.task('unit-test', ['coveralls']);
 gulp.task('e2e-test', ['protractor', 'disconnect']);
 gulp.task('test', ['unit-test', 'e2e-test']);
