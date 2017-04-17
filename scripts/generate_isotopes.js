96b06959fd48a83c1e7a79fe9bac34da326537d0
Andrés <angarg12@gmail.com>
2017-04-17 15:40:41 +0200

    First pass at isotope generation script

---
+++ scripts/generate_isotopes.js
@@ -0,0 +1,35 @@
+'use strict';
+
+var jsonfile = require('jsonfile');
+var path = require('path');
+
+var args = process.argv.slice(2);
+
+var base_path = path.join(args[0],'/data');
+
+var resources = jsonfile.readFileSync(path.join(base_path,'/resources.json'));
+var elements = jsonfile.readFileSync(path.join(base_path,'/elements.json'));
+
+for(var element in elements){
+  elements[element].includes = elements[element].includes || [];
+
+  var isotopes = elements[element].isotopes;
+  for(var isotope in isotopes){
+    resources[isotope] = {};
+    resources[isotope].ratio = isotopes[isotope].ratio;
+    delete isotopes[isotope].ratio;
+    resources[isotope].decay = isotopes[isotope].decay;
+    delete isotopes[isotope].decay;
+    resources[isotope].elements = [element];
+    resources[isotope].html = "<sup>"+numberPrefix(isotope)+"</sup>"+element;
+
+    elements[element].includes.push(isotope);
+  }
+}
+
+function numberPrefix(string){
+  return string.replace(/(^\d+)(.+$)/i,'$1');
+}
+
+jsonfile.writeFileSync(path.join(base_path,'/resources.json'), resources, {spaces:2});
+jsonfile.writeFileSync(path.join(base_path,'/elements.json'), elements, {spaces:2});
