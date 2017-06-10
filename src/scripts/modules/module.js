b72a162089b2594a79bf21e2683f6bc6fe4035dd
Andrés <angarg12@gmail.com>
2017-06-10 16:59:48 +0200

    Table now uses element cards

--- src/scripts/modules/module.js
+++ src/scripts/modules/module.js
@@ -1,3 +1,11 @@
 'use strict';
 
-angular.module('game', ['ngAnimate', 'ui.router']);
+angular.module('game', ['ngAnimate', 'ui.router'])
+.filter('keylength', function(){
+  return function(input){
+    if(!angular.isObject(input)){
+      throw Error('Usage of non-objects with keylength filter!!');
+    }
+    return Object.keys(input).length;
+  };
+});
