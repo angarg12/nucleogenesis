8606db094df28cfd771e41abd061236f9a831a88
Andrés <angarg12@gmail.com>
2017-08-31 21:05:36 +0200

    Closes #91

--- build_scripts/generate_decay.js
+++ build_scripts/generate_decay.js
@@ -9,8 +9,31 @@ let args = process.argv.slice(2);
 let resources = jsonfile.readFileSync(args[0] + '/data/resources.json');
 let elements = jsonfile.readFileSync(args[0] + '/data/elements.json');
 
+let isotopeMatrix = {};
+
+function getAtomBreakdown(isotope) {
+  let element = isotope.replace(/^[0-9]*/, '');
+  let number = parseInt(isotope.replace(element, ''));
+  let elementNumber = elements[element].number;
+  let neutronNumber = number - elementNumber;
+  return {
+    atom: elementNumber,
+    n: neutronNumber
+  };
+}
+
 for (let element in elements) {
-  for(let key in elements[element].isotopes){
+  for (let isotope in elements[element].isotopes) {
+    let breakdown = getAtomBreakdown(isotope);
+    if (!isotopeMatrix[breakdown.atom]) {
+      isotopeMatrix[breakdown.atom] = {};
+    }
+    isotopeMatrix[breakdown.atom][breakdown.n] = isotope;
+  }
+}
+
+for (let element in elements) {
+  for (let key in elements[element].isotopes) {
     let isotope = elements[element].isotopes[key];
     if (isotope.decay) {
       let ratioSum = 0;
@@ -41,8 +64,7 @@ function generateReaction(isotope, type) {
   case 'alpha':
     return calculateReaction(isotope, 1, 'He2+', -2, -4);
   case 'SF':
-    // FIXME: not yet implemented
-    return {};
+    return calculateSF(isotope);
   default:
     throw new Error('Unrecognized decay type: ' + type);
   }
@@ -50,7 +72,7 @@ function generateReaction(isotope, type) {
 
 function calculateReaction(isotope, number, particle, protonDifference, isotopeDifference) {
   let element = isotope.replace(/^[0-9]*/, '');
-  let elementNumber = elements[element].number-1;
+  let elementNumber = elements[element].number - 1;
   let listElements = Object.keys(elements);
   let otherElement = listElements[elementNumber + protonDifference];
 
@@ -61,57 +83,100 @@ function calculateReaction(isotope, number, particle, protonDifference, isotopeD
   // essentially we look for the closest isotope to the target one, and consume/produce
   // free neutrons in the process
   let distance = 0;
-  if(!resources[product]){
+  if (!resources[product]) {
     let candidate = null;
     // first we start looking for lighter isotopes
-    for(let otherNumber = isotopeNumber; otherNumber > 0; otherNumber--){
+    for (let otherNumber = isotopeNumber; otherNumber > 0; otherNumber--) {
       let otherProduct = otherNumber + otherElement;
-      if(resources[otherProduct]){
+      if (resources[otherProduct]) {
         candidate = otherProduct;
-        distance = isotopeNumber-otherNumber;
+        distance = isotopeNumber - otherNumber;
         break;
       }
     }
     // pay attention to the upper bound. 300 is bigger than any known isotope, so it is safe
-    for(let otherNumber = isotopeNumber; otherNumber < 300; otherNumber++){
+    for (let otherNumber = isotopeNumber; otherNumber < 300; otherNumber++) {
       let otherProduct = otherNumber + otherElement;
-      if(resources[otherProduct]){
-      // we only replace the candidate if the distance is smaller
-      if(isotopeNumber-otherNumber < Math.abs(distance)){
+      if (resources[otherProduct]) {
+        // we only replace the candidate if the distance is smaller
+        if (isotopeNumber - otherNumber < Math.abs(distance)) {
           candidate = otherProduct;
-          distance = isotopeNumber-otherNumber;
+          distance = isotopeNumber - otherNumber;
         }
         break;
       }
     }
-    if(!candidate){
-      throw new Error('No candidate found for '+isotope+' replacing the missing isotope '+product);
+    if (!candidate) {
+      throw new Error('No candidate found for ' + isotope + ' replacing the missing isotope ' + product);
     }
     product = candidate;
   }
   let energy = resources[isotope].energy - resources[product].energy;
-  if(distance < 0){
-    energy -= resources.n.energy*distance;
+  if (distance < 0) {
+    energy -= resources.n.energy * distance;
   }
 
   let reaction = {};
   reaction.reactant = {};
   reaction.reactant[isotope] = number;
   // if the isotope is heavier, the distance is negative and we produce neutrons
-  if(distance < 0){
+  if (distance < 0) {
     reaction.reactant.n = Math.abs(distance);
   }
   reaction.product = {};
   reaction.product[product] = number;
-  if(particle){
+  if (particle) {
     reaction.product[particle] = number;
   }
   // if the isotope is lighter, the distance is positive and we produce neutrons
-  if(distance > 0){
+  if (distance > 0) {
     reaction.product.n = distance;
   }
   reaction.product.eV = energy;
 
+  return reaction;
+}
+
+function calculateSF(isotope) {
+  let breakdown = getAtomBreakdown(isotope);
+  let halfNeutron = Math.floor(breakdown.n / 2);
+  let halfAtom = Math.floor(breakdown.atom / 2);
+  let firstHalf;
+  for (let i = halfNeutron; i > 0; i--) {
+    if (isotopeMatrix[halfAtom][i]) {
+      firstHalf = isotopeMatrix[halfAtom][i];
+      break;
+    }
+  }
+  let secondHalfNeutron = breakdown.n - halfNeutron;
+  let secondHalfAtom = breakdown.atom - halfAtom;
+
+  let secondHalf;
+  for (let i = secondHalfNeutron; i > 0; i--) {
+    if (isotopeMatrix[secondHalfAtom][i]) {
+      secondHalf = isotopeMatrix[secondHalfAtom][i];
+      break;
+    }
+  }
+  let firstHalfBreakdown = getAtomBreakdown(firstHalf);
+  let secondHalfBreakdown = getAtomBreakdown(secondHalf);
+  let neutronExcess = breakdown.n - firstHalfBreakdown.n - secondHalfBreakdown.n;
+
+  let energy = resources[isotope].energy - resources[firstHalf].energy -
+    resources[secondHalf].energy - resources.n.energy * neutronExcess;
+
+  let reaction = {};
+  reaction.reactant = {};
+  reaction.reactant[isotope] = 1;
+  reaction.product = {};
+  reaction.product[firstHalf] = 1;
+  reaction.product[secondHalf] = reaction.product[secondHalf]+1 || 1;
+  if (neutronExcess > 0) {
+    reaction.product.n = neutronExcess;
+  }
+  if (energy > 0) {
+    reaction.product.eV = energy;
+  }
   return reaction;
 }
 
