4b4b1e548eda1725ca0b2645f7e381074835ca80
HeinousTugboat <admin@heinousindustries.com>
2017-09-07 19:52:10 -0400

    fix: adds Objects.values polyfill to support older browsers
    Mostly so I can actually run the game in Firefox 45 ESR without having to monkey patch it every page load.

--- src/scripts/controllers/main-loop.js
+++ src/scripts/controllers/main-loop.js
@@ -13,6 +13,35 @@ angular
       let self = this;
       let playerCopy = null;
 
+      /**
+       * Polyfill for Object.values from the TS39 Proposal. Reflect.ownKeys
+       * swapped out for an equivalent using Object instead. Placing it here so
+       * it'll fire before it's needed in ct_matter, and at the top of this
+       * controller so it'll only fire once.
+       *
+       * Polyfill: https://github.com/tc39/proposal-object-values-entries
+       * Reflect.ownKeys Alternative:
+       * https://stackoverflow.com/questions/34449045/what-is-the-difference-between-reflect-ownkeysobj-and-object-keysobj
+       */
+      const reduce = Function.bind.call(Function.call, Array.prototype.reduce);
+      const isEnumerable = Function.bind.call(Function.call, Object.prototype.propertyIsEnumerable);
+      const concat = Function.bind.call(Function.call, Array.prototype.concat);
+      const keys = (target) => Object.getOwnPropertyNames(target).concat(Object.getOwnPropertySymbols(target));
+      // const keys = Reflect.ownKeys;
+
+      if (!Object.values) {
+        Object.values = function values(O) {
+          return reduce(keys(O), (v, k) => concat(v, typeof k === 'string' && isEnumerable(O, k) ? [O[k]] : []), []);
+        };
+      }
+
+      if (!Object.entries) {
+        Object.entries = function entries(O) {
+          return reduce(keys(O), (e, k) => concat(e, typeof k === 'string' && isEnumerable(O, k) ? [[k, O[k]]] : []), []);
+        };
+      }
+      /** End Polyfill */
+
       self.update = function() {
         // do the update in a copy
         playerCopy = angular.copy(state.player);
