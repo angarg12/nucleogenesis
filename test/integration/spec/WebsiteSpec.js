510cd3245de27bf380c7b7d4043de31c9ea22372
Andrés <angarg12@gmail.com>
2017-10-28 11:35:05 +0200

    Fixed e2e test

--- test/integration/spec/WebsiteSpec.js
+++ test/integration/spec/WebsiteSpec.js
@@ -1,3 +1,7 @@
+/* globals describe,it,expect,beforeEach, require, browser, element, by */
+/* jshint varstmt: false */
+'use strict';
+
 describe('Game index', function() {
   let player;
   let LocalStorage = require("../helper/LocalStorage.js");
@@ -11,13 +15,12 @@ describe('Game index', function() {
     // we need to load the page to set the player and load it again to load the save
     browser.get('http://localhost:9000/index.html');
 
-    player.elements.H.generators["1"] = 10;
-    player.elements.H.generators["2"] = 10;
-    player.elements.H.generators["5"] = 5;
+    player.element_slots[0].generators = {'1': 10, '2': 10, '5': 5};
+
     LocalStorage.setValue(JSON.stringify(player));
 
     browser.get('http://localhost:9000/index.html');
 
-    expect(element(by.id('production_total')).getText()).toEqual('13,110');
+    expect(element(by.id('production_total')).getText()).toEqual('310');
   });
 });
