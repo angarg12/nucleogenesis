74c947c86a670261efd8208233b7ef7200b2449a
Andrés <angarg12@gmail.com>
2017-12-03 14:03:25 +0100

    Fixed broken tests

--- test/unit/spec/Elements.js
+++ test/unit/spec/Elements.js
@@ -11,7 +11,7 @@ describe('Elements component', function() {
   describe('purchase functions', function() {
     it('should not purchase element if roll is failed', function() {
       spec.state.player.options.elementBuyIndex = 0;
-      spec.data.elements.O = {abundance: 0.5};
+      spec.data.elements.O = {abundance: 0.5, includes: []};
       spec.state.player.elements_unlocked = 1;
       spec.state.player.resources.dark_matter = {number:1};
       spec.state.player.elements.O = false;
@@ -27,7 +27,7 @@ describe('Elements component', function() {
 
     it('should purchase element if roll is successful', function() {
       spec.state.player.options.elementBuyIndex = 0;
-      spec.data.elements.O = {abundance: 0.5};
+      spec.data.elements.O = {abundance: 0.5, includes: []};
       spec.state.player.elements_unlocked = 1;
       spec.state.player.resources.dark_matter = {number:1};
       spec.state.player.elements.O = false;
@@ -43,7 +43,7 @@ describe('Elements component', function() {
 
     it('should not purchase element if cost is not met', function() {
       spec.state.player.options.elementBuyIndex = 0;
-      spec.data.elements.O = {abundance: 1};
+      spec.data.elements.O = {abundance: 1, includes: []};
       spec.state.player.elements_unlocked = 2;
       spec.state.player.resources.dark_matter = {number:0};
       spec.state.player.elements.O = false;
@@ -56,7 +56,7 @@ describe('Elements component', function() {
     });
 
     it('should spend purchase amount', function() {
-      spec.data.elements.O = {abundance: 0.5};
+      spec.data.elements.O = {abundance: 0.5, includes: []};
       spec.state.player.elements_unlocked = 1;
       spec.state.player.resources.dark_matter = {number:10};
       spec.state.player.elements.O = false;
@@ -72,7 +72,7 @@ describe('Elements component', function() {
     });
 
     it('should not spend more than purchase amount', function() {
-      spec.data.elements.O = {abundance: 0.5};
+      spec.data.elements.O = {abundance: 0.5, includes: []};
       spec.state.player.elements_unlocked = 1;
       spec.state.player.resources.dark_matter = {number:10};
       spec.state.player.elements.O = false;
@@ -88,7 +88,7 @@ describe('Elements component', function() {
     });
 
     it('purchase amount should increase chance of success', function() {
-      spec.data.elements.O = {abundance: 0.1};
+      spec.data.elements.O = {abundance: 0.1, includes: []};
       spec.state.player.elements_unlocked = 1;
       spec.state.player.resources.dark_matter = {number:10};
       spec.state.player.elements.O = false;
