3795fc8c31e5e7f932a6088111b7a346273396b8
Andrés <angarg12@gmail.com>
2018-01-11 20:10:03 +0100

    Closes #171

--- build_scripts/populate_player.js
+++ build_scripts/populate_player.js
@@ -36,7 +36,7 @@ startPlayer.version = npm.version;
      *    - short
      */
 startPlayer.options = {};
-startPlayer.options.numberformat = {format: 'standard', flavor: 'short', maxSmall: '1', sigfigs: 4};
+startPlayer.options.numberformat = {format: 'scientific', flavor: 'short', maxSmall: '1', sigfigs: 4};
 startPlayer.options.buyIndex = 0;
 startPlayer.options.adjustIndex = 0;
 startPlayer.options.elementBuyIndex = 0;
