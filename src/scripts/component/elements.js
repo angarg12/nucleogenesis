814126f320d5c5d20f479f26443517112b78e819
Andrés <angarg12@gmail.com>
2018-01-16 18:06:55 +0100

    Closes #169

--- src/scripts/component/elements.js
+++ src/scripts/component/elements.js
@@ -9,11 +9,11 @@
 
 angular.module('game').component('elements', {
   templateUrl: 'views/elements.html',
-  controller: ['$timeout', 'state', 'data', 'util', elements],
+  controller: ['$timeout', 'state', 'data', 'util', 'visibility', elements],
   controllerAs: 'ct'
 });
 
-function elements($timeout, state, data, util) {
+function elements($timeout, state, data, util, visibility) {
   let ct = this;
   ct.state = state;
   ct.data = data;
@@ -24,47 +24,47 @@ function elements($timeout, state, data, util) {
 
   ct.getChance = function(element, player) {
     let bonus = 1;
-    for(let resource of data.elements[element].includes){
+    for (let resource of data.elements[element].includes) {
       let allTime = player.statistics.all_time[resource];
-      if(allTime){
-         bonus *= allTime*data.constants.ELEMENT_CHANCE_BONUS+1;
-       }
+      if (allTime) {
+        bonus *= allTime * data.constants.ELEMENT_CHANCE_BONUS + 1;
+      }
     }
 
-    let singleChance = data.elements[element].abundance*bonus;
-    let chance = 1 - Math.pow(Math.max(0, 1-singleChance),
-                              Math.min(player.resources.dark_matter.number, ct.buyAmount[player.options.elementBuyIndex]));
+    let singleChance = data.elements[element].abundance * bonus;
+    let chance = 1 - Math.pow(Math.max(0, 1 - singleChance),
+      Math.min(player.resources.dark_matter.number, ct.buyAmount[player.options.elementBuyIndex]));
 
     return Math.min(1, chance);
   };
 
-  ct.buyElement = function (element, player) {
+  ct.buyElement = function(element, player) {
     if (player.elements[element]) {
       return;
     }
-    if(Math.random() < ct.getChance(element, player)){
+    if (Math.random() < ct.getChance(element, player)) {
       player.elements[element] = true;
       player.exotic_upgrades[element] = {};
-      for(let up in data.exotic_upgrades){
+      for (let up in data.exotic_upgrades) {
         player.exotic_upgrades[element][up] = false;
       }
       player.elements_unlocked++;
       ct.outcome[element] = 'Success';
-    }else{
+    } else {
       ct.outcome[element] = 'Fail';
     }
     player.resources.dark_matter.number -= Math.min(player.resources.dark_matter.number, ct.buyAmount[player.options.elementBuyIndex]);
 
-    util.delayedExec(performance.now(),performance.now(), 1000, () => ct.clearMessage(element));
+    util.delayedExec(performance.now(), performance.now(), 1000, () => ct.clearMessage(element));
   };
 
-  ct.clearMessage = function (element) {
+  ct.clearMessage = function(element) {
     ct.outcome[element] = '';
   };
 
   /* This function returns the class that determines on which
   colour an element card */
-  ct.elementClass = function (element, player) {
+  ct.elementClass = function(element, player) {
     if (player.elements[element]) {
       return 'element_purchased';
     }
@@ -75,8 +75,8 @@ function elements($timeout, state, data, util) {
   };
 
   function isElementAvailable(element, player) {
-    for(let resource of data.elements[element].includes){
-      if(player.resources[resource].unlocked){
+    for (let resource of data.elements[element].includes) {
+      if (player.resources[resource].unlocked) {
         return true;
       }
     }
@@ -85,7 +85,24 @@ function elements($timeout, state, data, util) {
 
   /* This function returns the class that determines the secondary
   colour of an element card */
-  ct.elementSecondaryClass = function (element, player) {
+  ct.elementSecondaryClass = function(element, player) {
     return ct.elementClass(element, player) + '_dark';
   };
+
+  ct.visibleTableElements = function(player) {
+    return visibility.visible(data.elements, isTableElementVisible, null, null, player);
+  };
+
+  function isTableElementVisible(element, _, player) {
+    switch (ct.elementClass(element, player)) {
+      case 'element_purchased':
+        return !player.options.hideElementsPurchased;
+      case 'element_available':
+        return !player.options.hideElementsAvailable;
+      case 'element_unavailable':
+        return !player.options.hideElementsUnavailable;
+      default:
+        return true;
+    }
+  }
 }
